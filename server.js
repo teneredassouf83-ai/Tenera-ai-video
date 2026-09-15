fde5b7d52e8c0c6f1e73d303fdce79279df09da3
teneredassouf83-ai <teneredassouf83@gmail.com>
2026-09-15 15:49:49 +0000

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.post("/generate", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Tenera AI Video démarré sur le port 3000");
+
+    const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Tenera AI Video démarré sur le port ${PORT}`);
 });
+                
